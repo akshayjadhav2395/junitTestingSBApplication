9b0d01bd9b0d456059cda1584e5977232d304484
Akshay Jadhav <98974798+akshayjadhav2395@users.noreply.github.com>
2022-03-29 01:19:21 +0530

    @Repository Annotation Added..

--- src/main/java/com/example/demo/repository/UserRepo.java
+++ src/main/java/com/example/demo/repository/UserRepo.java
@@ -3,9 +3,13 @@ package com.example.demo.repository;
 import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Repository
 public interface UserRepo extends JpaRepository<User, Integer> {
+	
+	// Missing @Repository Annotation added
 
     @Query("SELECT CASE WHEN COUNT(u) > 0 THEN TRUE ELSE FALSE END FROM User u WHERE u.uid =:id")
     public boolean isUserExistByUid(@RequestParam int id);
